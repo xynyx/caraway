bd50d6ba8e648d07d1ad85578ef6391c147fc33d
juroden <juroden@users.noreply.github.com>
2024-03-10 13:48:29 -0400

    simplify setup

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12447c813a634e83ef85a810b0b8a20a711172d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ab362e98019a013ad78a4854ea587f7831da500")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
