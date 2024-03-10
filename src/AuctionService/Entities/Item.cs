e15990d1124eb550c1d13b15868c947d8e836b42
juroden <juroden@users.noreply.github.com>
2024-03-10 14:12:47 -0400

    Add basic entities

---
+++ src/AuctionService/Entities/Item.cs
@@ -0,0 +1,18 @@
+﻿using AuctionService.Entities;
+
+namespace AuctionService;
+
+public class Item
+{
+    public Guid Id { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; }
+    public int Mileage { get; set; }
+    public string ImageUrl { get; set; }
+
+    // "Nav properties" - Define relationship to Auction entity; configures schema/relationship for the DB
+    public Auction Auction { get; set; }
+    public Guid AuctionId { get; set; }
+}
--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ab362e98019a013ad78a4854ea587f7831da500")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd50d6ba8e648d07d1ad85578ef6391c147fc33d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
